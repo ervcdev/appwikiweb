cfd0becf813fb8c84be94365f8640b629d01be72
Ervin Castro Perez <118087055+ervcdev@users.noreply.github.com>
2023-06-04 17:17:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const whiteList = [process.env.ORIGIN1];
 app.use(
     cors({
         origin: function(origin, callback) {
-            if (whiteList.includes(origin)) {
+            if (!origin || whiteList.includes(origin)) {
                 return callback(null, origin);
             }
             return callback("No autorizado por CORS" );
